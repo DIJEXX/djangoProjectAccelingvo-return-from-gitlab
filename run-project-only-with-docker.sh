ae97908a35a5c8a0fe60c3e758d5297374694595
DIJEXX <daniilsologu6@gmail.com>
2024-07-20 21:37:06 +0000

    Update run-project-only-with-docker.sh

--- run-project-only-with-docker.sh
+++ run-project-only-with-docker.sh
@@ -1,4 +1,4 @@
 git clone https://gitlab.com/DIJEXX/djangoProjectAccelingvo.git
 cd djangoProjectAccelingvo
-docker build -t accelingvo .
-docker run -p 8080:8080 accelingvo
+sudo docker build -t accelingvo .
+sudo docker run -p 8080:8080 accelingvo
